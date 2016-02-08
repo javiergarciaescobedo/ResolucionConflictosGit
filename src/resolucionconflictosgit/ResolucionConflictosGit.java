2b9b9b89477565deefb72b0b1fb40122296a382d
Javier García Escobedo <correo@javiergarciaescobedo.es>
2016-02-08 21:20:40 +0100

    Línea añadida en PC2

--- src/resolucionconflictosgit/ResolucionConflictosGit.java
+++ src/resolucionconflictosgit/ResolucionConflictosGit.java
@@ -4,6 +4,7 @@ public class ResolucionConflictosGit {
 
     public static void main(String[] args) {
         System.out.println("Código escrito en PC1");
+        System.out.println("Línea añadida en PC2");
     }
     
 }
