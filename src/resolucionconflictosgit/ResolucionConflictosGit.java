989fc9a5bb7feb296d81d82505d7e83cd6037331
Javier García Escobedo <correo@javiergarciaescobedo.es>
2016-02-08 21:16:24 +0100

    Initial commit

---
+++ src/resolucionconflictosgit/ResolucionConflictosGit.java
@@ -0,0 +1,9 @@
+package resolucionconflictosgit;
+
+public class ResolucionConflictosGit {
+
+    public static void main(String[] args) {
+        System.out.println("Código escrito en PC1");
+    }
+    
+}
